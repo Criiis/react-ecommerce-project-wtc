d416a063821dc36a7875e1ceb906d1ade6a1cf44
Criiis <cristiano.luis@wundermanthompson.com>
2022-10-13 20:54:53 +0100

    fetching data

--- src/components/elements/PLP/Plp.js
+++ src/components/elements/PLP/Plp.js
@@ -1,8 +1,32 @@
 import './Plp.css'
 import Product from '../product/Product'
-import { productApi } from '../products-api'
+import { useEffect, useState } from 'react'
 
 export default function Plp() {
-  const outputData = productApi.map((product) => <Product product={product} />)
+  const [productApi, setProductApi] = useState([])
+
+  const fetchProductsAPI = async () => {
+    console.log('test 1 fetch')
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products`)
+      if (!response.ok) throw new Error('Could not fetch!')
+      const products = await response.json()
+
+      setProductApi(products)
+    } catch (error) {
+      throw new Error(`${error.message}, try again.`)
+    } finally {
+    }
+  }
+
+  useEffect(() => {
+    fetchProductsAPI()
+    console.log('test 2 fetch')
+  }, [])
+
+  const outputData = productApi?.map((product) => (
+    <Product key={product.id} product={product} />
+  ))
+
   return <div className="plp-container">{outputData}</div>
 }
