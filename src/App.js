5cbd782f963c45102ab70ce000c7fc90f139ae4c
Criiis <cristiano.luis@wundermanthompson.com>
2022-10-13 13:58:07 +0100

    create header style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Header from './components/elements/header/Header'
+
 function App() {
   return (
     <>
+      <Header />
       <h1>HELLO WORLD!</h1>
       <h2>this is a react app!</h2>
     </>
--- src/components/elements/header/Header.js
+++ src/components/elements/header/Header.js
@@ -1,3 +1,21 @@
+import './Header.css'
+import { WishlistIcon, CartIcon } from './icon'
+
 export default function Header() {
-  return <div>Header</div>
+  return (
+    <div className="header">
+      <h1>React Shop</h1>
+
+      <div class="nav">
+        <div className="nav--icon">
+          <WishlistIcon />
+          <p>Wishlist</p>
+        </div>
+        <div className="nav--icon">
+          <CartIcon />
+          <p>Cart</p>
+        </div>
+      </div>
+    </div>
+  )
 }
