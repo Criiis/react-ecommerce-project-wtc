348b1f1411bf661fe5a5f59fdf34b2de5f336da8
Criiis <cristiano.luis@wundermanthompson.com>
2022-10-16 20:41:46 +0100

    create cart provider

--- src/components/storage/CartProvider.js
+++ src/components/storage/CartProvider.js
@@ -11,8 +11,52 @@ const REMOVECART = 'REMOVE_FROM_CAR'
 const CLEARCART = 'CLEAR_CART'
 
 function cartReducer(state, action) {
-  console.log(state, action)
-  return state
+  console.log('state', state)
+  console.log('action', action)
+  switch (action.type) {
+    case ADDCART: {
+      const stateProducts = state.products //current state product
+      const statePrice = state.totalAmount //current price in state
+      const productState = action.payload.item //product added
+      const productPrice = productState.price //product price
+      const updateTotalPrice = statePrice + productPrice //total price update
+      let updateProduct = []
+
+      const indexOfProduct = stateProducts.findIndex(
+        (el) => el.id === productState.id
+      )
+
+      if (indexOfProduct === -1) {
+        updateProduct = [
+          ...stateProducts,
+          { ...productState, quantity: action.payload.quantity },
+        ]
+      } else {
+        stateProducts[indexOfProduct].quantity =
+          stateProducts[indexOfProduct].quantity + action.payload.quantity
+        updateProduct = [...stateProducts]
+      }
+
+      console.log(updateProduct)
+      return { products: updateProduct, totalAmount: updateTotalPrice }
+    }
+    case REMOVECART: {
+      const stateProducts = state.products //current state product
+      const statePrice = state.totalAmount //current price in state
+      const productState = action.payload.item //product added
+      const productPrice = productState.price //product price
+      const updateTotalPrice = statePrice - productPrice //total price update
+
+      console.log(REMOVECART)
+      return { products: stateProducts, totalAmount: updateTotalPrice }
+    }
+    case CLEARCART: {
+      console.log(CLEARCART)
+      return state
+    }
+    default:
+      return state
+  }
 }
 
 export default function CartProvider(props) {
