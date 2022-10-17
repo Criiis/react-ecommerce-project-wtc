cbd318149ff75879dd6f80be661e7c5a31463b81
Criiis <cristiano.luis@wundermanthompson.com>
2022-10-17 23:14:52 +0100

    update cart modules

--- src/components/elements/cart/Cart.js
+++ src/components/elements/cart/Cart.js
@@ -1,15 +1,36 @@
 import { useContext } from 'react'
 import CartContext from '../../storage/CartContext'
+import { transformToCurrency } from '../helpers'
 import Modal from '../modal/Modal'
 
 export default function Cart({ cartHandler }) {
-  const { totalAmount, products } = useContext(CartContext)
+  const {
+    totalAmount,
+    products,
+    clearProductsFromCart,
+    removeProductFromCart,
+  } = useContext(CartContext)
 
+  // function removeProduct(e) {
+  //   e.preventDefault()
+  //   removeProductFromCart(product.id)
+  // }
+
+  //product card for cart modal
   const productsOutput = products?.map((product) => {
     return (
-      <h1>
-        {product.title} x{product.quantity}
-      </h1>
+      <div className="product-card" key={product.id}>
+        <div className="product-card--images">
+          <img src={product.image} alt={product.title} />
+        </div>
+        <p>{product.title}</p>
+        <p>
+          {transformToCurrency(product.price)} x{product.quantity}
+        </p>
+        <button onClick={() => removeProductFromCart(product.id)}>
+          Remove Item
+        </button>
+      </div>
     )
   })
 
@@ -17,8 +38,8 @@ export default function Cart({ cartHandler }) {
     <Modal clickHandler={cartHandler}>
       <h1>Cart</h1>
       {productsOutput}
-      <p>total: {totalAmount}</p>
-      <button>Clear All</button>
+      <p>total: {transformToCurrency(totalAmount)}</p>
+      <button onClick={clearProductsFromCart}>Clear All</button>
     </Modal>
   )
 }
