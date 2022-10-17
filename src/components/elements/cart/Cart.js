333afb71f98fb50e4bec307e8bbb9fca2772bf00
Criiis <cristiano.luis@wundermanthompson.com>
2022-10-17 18:47:28 +0100

    create cart

--- src/components/elements/cart/Cart.js
+++ src/components/elements/cart/Cart.js
@@ -3,11 +3,29 @@ import CartContext from '../../storage/CartContext'
 import Modal from '../modal/Modal'
 
 export default function Cart({ cartHandler }) {
-  const test = useContext(CartContext)
-  console.log(test)
+  const { totalAmount, products } = useContext(CartContext)
+
+  const productsOutput = products?.map((product) => {
+    return (
+      <h1>
+        {product.title} x{product.quantity}
+      </h1>
+    )
+  })
+
   return (
     <Modal clickHandler={cartHandler}>
       <h1>Cart</h1>
+      {productsOutput}
+      <p>total: {totalAmount}</p>
+      <button>Clear All</button>
     </Modal>
   )
 }
+
+//
+// addProductToCart
+// clearProductsFromCart
+// products
+// removeProductFromCart
+// totalAmount
