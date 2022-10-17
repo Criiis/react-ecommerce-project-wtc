fddb073f659fe98cfdd0e41db811776eabc34b3a
Criiis <cristiano.luis@wundermanthompson.com>
2022-10-17 23:14:36 +0100

    remove item functionality

--- src/components/storage/CartProvider.js
+++ src/components/storage/CartProvider.js
@@ -11,8 +11,6 @@ const REMOVECART = 'REMOVE_FROM_CAR'
 const CLEARCART = 'CLEAR_CART'
 
 function cartReducer(state, action) {
-  console.log('state', state)
-  console.log('action', action)
   switch (action.type) {
     case ADDCART: {
       const stateProducts = state.products //current state product
@@ -26,33 +24,53 @@ function cartReducer(state, action) {
         (el) => el.id === productState.id
       )
 
+      //if doesn't exist add first 1
       if (indexOfProduct === -1) {
         updateProduct = [
           ...stateProducts,
           { ...productState, quantity: action.payload.quantity },
         ]
       } else {
+        console.log(stateProducts[indexOfProduct].quantity)
+        console.log(action.payload.quantity)
+
         stateProducts[indexOfProduct].quantity =
           stateProducts[indexOfProduct].quantity + action.payload.quantity
         updateProduct = [...stateProducts]
       }
 
-      console.log(updateProduct)
       return { products: updateProduct, totalAmount: updateTotalPrice }
     }
     case REMOVECART: {
       const stateProducts = state.products //current state product
       const statePrice = state.totalAmount //current price in state
-      const productState = action.payload.item //product added
-      const productPrice = productState.price //product price
-      const updateTotalPrice = statePrice - productPrice //total price update
 
-      console.log(REMOVECART)
+      const payloadId = action.payload //product added
+      const indexOfProduct = stateProducts.findIndex(
+        (el) => el.id === payloadId
+      )
+
+      //if for some reason product doesn't exist return normal state
+      if (indexOfProduct === -1)
+        return { products: stateProducts, totalAmount: statePrice }
+
+      const currentProduct = stateProducts[indexOfProduct]
+
+      //if quantity of the product is 1 meaning u will remove the item completely from cart
+      if (currentProduct.quantity === 1) stateProducts.splice(indexOfProduct, 1)
+      else
+        stateProducts[indexOfProduct].quantity =
+          stateProducts[indexOfProduct].quantity - 1
+
+      console.log(stateProducts)
+      //update price
+      const updateTotalPrice =
+        stateProducts.length === 0 ? 0 : statePrice - currentProduct.price
+
       return { products: stateProducts, totalAmount: updateTotalPrice }
     }
     case CLEARCART: {
-      console.log(CLEARCART)
-      return state
+      return { products: [], totalAmount: 0 }
     }
     default:
       return state
