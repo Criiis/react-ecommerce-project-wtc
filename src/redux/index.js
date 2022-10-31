f1e855270cbfdb4d81d70945e8019f2f7e2ee33f
Criiis <cristiano.luis@wundermanthompson.com>
2022-10-31 00:57:47 +0000

    create export for redux

---
+++ src/redux/index.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from './cart'
+import wishlistReducer from './wishlist'
+
+const store = configureStore({
+  reducer: {
+    cartReducer,
+    wishlistReducer,
+  },
+})
