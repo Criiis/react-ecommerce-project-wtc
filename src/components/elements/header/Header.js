d29152a7139c4dff326fc41afa6b111f82667317
Criiis <cristiano.luis@wundermanthompson.com>
2023-01-10 20:59:14 +0000

    add ul to nav

--- src/components/elements/header/Header.js
+++ src/components/elements/header/Header.js
@@ -6,26 +6,28 @@ export default function Header({ wishlistHandler, cartHandler }) {
     <div className={styles.header}>
       <h1>React Shop</h1>
 
-      <nav className={styles.nav}>
-        <li
-          className={styles.navIcon}
-          onClick={wishlistHandler}
-          aria-label="open wishlist section"
-          role="button"
-        >
-          <WishlistIcon className={styles.iconCounter} />
-          <p aria-hidden="true">Wishlist</p>
-        </li>
+      <nav>
+        <ul className={styles.nav}>
+          <li
+            className={styles.navIcon}
+            onClick={wishlistHandler}
+            aria-label="open wishlist section"
+            role="button"
+          >
+            <WishlistIcon className={styles.iconCounter} />
+            <p aria-hidden="true">Wishlist</p>
+          </li>
 
-        <li
-          className={styles.navIcon}
-          onClick={cartHandler}
-          aria-label="open cart section"
-          role="button"
-        >
-          <CartIcon className={styles.iconCounter} />
-          <p aria-hidden="true">Cart</p>
-        </li>
+          <li
+            className={styles.navIcon}
+            onClick={cartHandler}
+            aria-label="open cart section"
+            role="button"
+          >
+            <CartIcon className={styles.iconCounter} />
+            <p aria-hidden="true">Cart</p>
+          </li>
+        </ul>
       </nav>
     </div>
   )
