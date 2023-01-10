1b8db1ec081140a914b7bf6a75fb42c548578f58
Criiis <cristiano.luis@wundermanthompson.com>
2023-01-10 22:53:13 +0000

    helper as ts

---
+++ src/components/elements/helpers.ts
@@ -0,0 +1,18 @@
+/**
+ *
+ * @param {*} value should be a number
+ * @returns return the number as currency
+ */
+export const transformToCurrency = (value: string | number) => {
+  const valueToNumber = typeof value === 'string' ? +value.trim() : +value
+
+  if (isNaN(valueToNumber))
+    return console.warn(
+      `Something is wrong with ur transformToCurrency value ${valueToNumber}.`
+    )
+
+  return new Intl.NumberFormat('default', {
+    style: 'currency',
+    currency: 'GBP',
+  }).format(valueToNumber)
+}
