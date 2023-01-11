ae047a83a4e6790936cb91ee2275cdebfaee65cf
Criiis <cristiano.luis@wundermanthompson.com>
2023-01-11 00:26:25 +0000

    PLP to TS

--- src/components/elements/PLP/Plp.tsx
+++ src/components/elements/PLP/Plp.tsx
@@ -1,20 +1,21 @@
+import { useEffect, useState } from 'react'
 import Product from '../product/Product'
 import styles from './Plp.module.css'
 import Loader from '../loader/Loader'
-import { useEffect, useState } from 'react'
+import { product } from '../../../state'
 
 export default function Plp() {
   const [loading, setLoading] = useState(true)
-  const [productApi, setProductApi] = useState([])
+  const [productApi, setProductApi] = useState<product[]>([])
 
   const fetchProductsAPI = async () => {
     try {
       const response = await fetch(`https://fakestoreapi.com/products`)
       if (!response.ok) throw new Error('Could not fetch!')
-      const products = await response.json()
+      const products: product[] = await response.json()
 
       setProductApi(products)
-    } catch (error) {
+    } catch (error: any) {
       throw new Error(`${error.message}, try again.`)
     } finally {
       setLoading(false)
