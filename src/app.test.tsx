e84e3296a6f60c06914719ba5bee56e0d33d85fc
Criiis <cristiano.luis@wundermanthompson.com>
2023-01-14 23:26:14 +0000

    trying to fix testing line 29 broken

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -3,28 +3,43 @@ import { render, screen } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './redux/index'
+import userEvent from '@testing-library/user-event'
 
-it('renders welcome message', async () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  )
+describe('Test full app', () => {
+  test('testing wishlist functionality', async () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+    const user = userEvent.setup()
 
-  // click add to cart
-  // check if product has been added to cart
+    // click in the product to add to the wishlist
+    const listOfWishlistButtons = await screen.findAllByRole('button', {
+      name: /Click to add to wishlist/,
+    })
+    expect(listOfWishlistButtons[0]).toBeInTheDocument()
+    await user.click(listOfWishlistButtons[0])
 
-  // click in the cart icon and open cart pop-up
-  // click clear all
+    // click in the wishlist icon and open wishlist pop-up
+    const wishlistButton = screen.getByRole('button', {
+      name: /open wishlist/i,
+    })
+    expect(wishlistButton).toBeInTheDocument()
+    await user.click(wishlistButton)
 
+    // check if the wishlist popup exist in the page
+    const wishlistHeading = screen.getByRole('heading', {
+      name: /Wishlist/,
+    })
+    expect(wishlistHeading).toBeInTheDocument()
 
+    // button
 
+    // click add to cart
+    // check if product has been added to cart
 
-  // click add to wishlist
-  // check if product has been added to wishlist
-
-  // click in the wishlist icon and open wishlist pop-up
-  
-
-
+    // click in the cart icon and open cart pop-up
+    // click clear all
+  })
 })
