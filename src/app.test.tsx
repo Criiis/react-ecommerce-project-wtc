0314ce9d5a005d8551c0051490855e5686216927
Criiis <cristiano.luis@wundermanthompson.com>
2023-01-17 22:18:55 +0000

    fully testing the app

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './redux/index'
@@ -21,11 +21,22 @@ describe('Test full app', () => {
     expect(listOfWishlistButtons[0]).toBeInTheDocument()
     await user.click(listOfWishlistButtons[0])
 
+    //check if product was added
+    expect(listOfWishlistButtons[0]).toHaveAttribute(
+      'aria-label',
+      'Product added to wishlist click to remove from wishlist'
+    )
+
     // click in the wishlist icon and open wishlist pop-up
     const wishlistButton = screen.getByRole('button', {
-      name: /open wishlist/i,
+      name: /open wishlist section/i,
     })
     expect(wishlistButton).toBeInTheDocument()
+    expect(wishlistButton).toHaveAttribute('role', 'button')
+    expect(wishlistButton).toHaveAttribute(
+      'aria-label',
+      'open wishlist section'
+    )
     await user.click(wishlistButton)
 
     // check if the wishlist popup exist in the page
@@ -34,12 +45,53 @@ describe('Test full app', () => {
     })
     expect(wishlistHeading).toBeInTheDocument()
 
-    // button
+    //close the popup
+    const backgroundButton = screen.getByRole('button', {
+      name: /click to close the section/i,
+    })
+    expect(backgroundButton).toBeInTheDocument()
+    await user.click(backgroundButton)
+  })
 
-    // click add to cart
-    // check if product has been added to cart
+  test('testing cart functionality', async () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+    const user = userEvent.setup()
 
-    // click in the cart icon and open cart pop-up
-    // click clear all
+    // click in the product to add to the cart
+    const listOfCartButtons = await screen.findAllByRole('button', {
+      name: /Add to cart/,
+    })
+    expect(listOfCartButtons[1]).toBeInTheDocument()
+    await user.click(listOfCartButtons[1])
+
+    // check if product was added
+    expect(listOfCartButtons[1]).toHaveTextContent('Product added')
+
+    // click in the Cart icon and open Cart pop-up
+    const cartButton = screen.getByRole('button', {
+      name: /open cart section/i,
+    })
+    expect(cartButton).toBeInTheDocument()
+    expect(cartButton).toHaveAttribute('role', 'button')
+    expect(cartButton).toHaveAttribute('aria-label', 'open cart section')
+    await user.click(cartButton)
+
+    // check if the Cart popup exist in the page
+    const CartHeading = screen.getByRole('heading', {
+      name: /Cart/,
+    })
+    expect(CartHeading).toBeInTheDocument()
+
+    //close the popup
+    const backgroundButton = screen.getByRole('button', {
+      name: /click to close the section/i,
+    })
+    expect(backgroundButton).toBeInTheDocument()
+    await user.click(backgroundButton)
   })
 })
+// https://dev.to/believer/testing-react-createportal-with-testing-library-1mj6
